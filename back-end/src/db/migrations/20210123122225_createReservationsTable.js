8444953fcd3470dad924116dc7fb3046e6c0ede1
Bradley Wong <blwong77@gmail.com>
2022-03-04 11:00:22 -0800

    updated schema for reservations table

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("firstName").notNullable();
+    table.string("lastName").notNullable();
+    table.string("mobileNumber").notNullable();
+    table.string("reservationDate").notNullable();
+    table.string("reservationTime").notNullable();
+    table.integer("people").notNullable();
     table.timestamps(true, true);
   });
 };
