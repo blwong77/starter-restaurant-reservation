da52064fbc0144d2c0db4830b85b41f53aa23f95
Bradley Wong <blwong77@gmail.com>
2022-03-07 14:16:31 -0800

    added validation for required reservation form fields

---
+++ back-end/src/reservations/validation/checkMobileNumber.js
@@ -0,0 +1,11 @@
+function checkMobileNumber(req, res, next) {
+  const mobileNumber = req.body.data.mobile_number;
+
+  if (mobileNumber) {
+    res.locals.reservationData.mobile_number = mobileNumber;
+    return next();
+  }
+  next({ status: 404, message: "Mobile number is required" });
+}
+
+module.exports = checkMobileNumber;
