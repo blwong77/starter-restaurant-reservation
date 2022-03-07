76f3ac51a8d5c29caadae694750b095bc2a9c0eb
Bradley Wong <blwong77@gmail.com>
2022-03-07 13:19:00 -0800

    updated column names

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
-    table.string("firstName").notNullable();
-    table.string("lastName").notNullable();
-    table.string("mobileNumber").notNullable();
-    table.string("reservationDate").notNullable();
-    table.string("reservationTime").notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.string("reservation_date").notNullable();
+    table.string("reservation_time").notNullable();
     table.integer("people").notNullable();
     table.timestamps(true, true);
   });
