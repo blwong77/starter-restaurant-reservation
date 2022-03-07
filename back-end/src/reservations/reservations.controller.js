b42d91bdd8c7d8cc71d793a86f5981dd8bc65f7f
Bradley Wong <blwong77@gmail.com>
2022-03-07 14:18:37 -0800

    added controller functions for list and create

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,27 +1,41 @@
-const service = require("./reservations.service")
+const service = require("./reservations.service");
 
 /**
  * Validation Imports
  */
-const checkFirstName = require("./validation/checkFirstName")
-const checkLastName = require("./validation/checkLastName")
+const checkFirstName = require("./validation/checkFirstName");
+const checkLastName = require("./validation/checkLastName");
+const checkMobileNumber = require("./validation/checkMobileNumber");
+const checkPeople = require("./validation/checkPeople");
+const checkTime = require("./validation/checkTime");
+const checkDate = require("./validation/checkDate");
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
+  const data = await service.getAllReservations()
   res.json({
-    data: [],
+    data,
   });
 }
 
 async function create(req, res) {
   const reservationData = res.locals.reservationData;
-  console.log(reservationData)
-  // await service.create(req.body.data);
+  console.log(reservationData);
+  const newReservation = await service.createReservation(reservationData);
+  res.status(201).json({data: newReservation});
 }
 
 module.exports = {
   list,
-  create:[checkFirstName, checkLastName, create],
+  create: [
+    checkFirstName,
+    checkLastName,
+    checkMobileNumber,
+    checkPeople,
+    checkTime,
+    checkDate,
+    create,
+  ],
 };
