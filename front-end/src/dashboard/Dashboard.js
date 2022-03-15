b1519989e9626275acd2608f8bc01e9a0eec1dfa
Bradley Wong <blwong77@gmail.com>
2022-03-15 10:35:48 -0700

    added a reservations table

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router";
+import { next, previous } from "../utils/date-time";
+import ReservationsTable from "./ReservationsTable/ReservationsTable";
 
 /**
  * Defines the dashboard page.
@@ -11,6 +14,7 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const history = useHistory();
 
   useEffect(loadDashboard, [date]);
 
@@ -23,15 +27,43 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  const handleClick = ({ target }) => {
+    switch (target.id) {
+      case "Previous":
+        history.push(`/dashboard?date=${previous(date)}`);
+        break;
+      case "Today":
+        history.push(`/dashboard`);
+        break;
+      case "Next":
+        history.push(`/dashboard?date=${next(date)}`);
+        break;
+      default:
+    }
+  };
+
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for {date}</h4>
-      </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
-    </main>
+    <div className="row">
+      <main>
+        <h1>Dashboard</h1>
+        <div className="d-md-flex mb-3">
+          <h4 className="mb-0">Reservations for {date}</h4>
+        </div>
+        <div className="mb-3">
+          <button id="Previous" type="btn" onClick={handleClick}>
+            Previous
+          </button>
+          <button id="Today" type="btn" onClick={handleClick}>
+            Today
+          </button>
+          <button id="Next" type="btn" onClick={handleClick}>
+            Next
+          </button>
+        </div>
+        <ErrorAlert error={reservationsError} />
+        <ReservationsTable reservations={reservations} />
+      </main>
+    </div>
   );
 }
 
