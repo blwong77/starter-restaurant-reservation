cc8164c25a5e4e528662387474144b8156101e52
Bradley Wong <blwong77@gmail.com>
2022-03-15 10:35:10 -0700

    created a component to render a reservations table on the dashboard page

---
+++ front-end/src/dashboard/ReservationsTable/ReservationsTable.js
@@ -0,0 +1,47 @@
+import React from "react";
+import TableData from "../common/TableData";
+import TableHeaders from "../common/TableHeaders";
+
+export default function ReservationsTable({ reservations }) {
+  
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <TableHeaders
+              headerCols={["#", "Name", "Phone", "Date", "Time", "People"]}
+            />
+          </tr>
+        </thead>
+        <tbody>
+          {reservations.map((reservation) => {
+            const {
+              reservation_id,
+              first_name,
+              last_name,
+              mobile_number,
+              reservation_date,
+              reservation_time,
+              people,
+            } = reservation;
+            return (
+              <tr>
+                <TableData
+                  tableData={[
+                    reservation_id,
+                    `${last_name}, ${first_name}`,
+                    mobile_number,
+                    reservation_date,
+                    reservation_time,
+                    people,
+                  ]}
+                />
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
