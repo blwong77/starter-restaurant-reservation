2b2f5ab97c7310efd391680f58b5c66447888185
Bradley Wong <blwong77@gmail.com>
2022-03-16 10:23:11 -0700

    changed handleInput to handle people as numbers instead of strings

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -17,10 +17,15 @@ export default function ReservationForm() {
   });
 
   const handleInput = ({ target }) => {
-    setReservationFormData({
-      ...reservationFormData,
-      [target.id]: target.value,
-    });
+    target.id === "people"
+      ? setReservationFormData({
+          ...reservationFormData,
+          [target.id]: Number(target.value),
+        })
+      : setReservationFormData({
+          ...reservationFormData,
+          [target.id]: target.value,
+        });
   };
 
   const handleCancel = () => {
@@ -46,6 +51,7 @@ export default function ReservationForm() {
             name="first_name"
             id="first_name"
             onChange={handleInput}
+            value={reservationFormData.first_name}
             required
           />
           <p>Last Name:</p>
@@ -54,6 +60,7 @@ export default function ReservationForm() {
             name="last_name"
             id="last_name"
             onChange={handleInput}
+            value={reservationFormData.last_name}
             required
           />
           <p>Mobile Number:</p>
@@ -62,6 +69,7 @@ export default function ReservationForm() {
             name="mobile_number"
             id="mobile_number"
             onChange={handleInput}
+            value={reservationFormData.mobile_number}
             required
           />
           <p>Reservation Date:</p>
@@ -70,6 +78,7 @@ export default function ReservationForm() {
             name="reservation_date"
             id="reservation_date"
             onChange={handleInput}
+            value={reservationFormData.reservation_date}
             required
           />
           <p>Reservation Time:</p>
@@ -78,6 +87,7 @@ export default function ReservationForm() {
             name="reservation_time"
             id="reservation_time"
             onChange={handleInput}
+            value={reservationFormData.reservation_time}
             required
           />
           <p>People:</p>
@@ -86,6 +96,7 @@ export default function ReservationForm() {
             name="people"
             id="people"
             onChange={handleInput}
+            value={reservationFormData.people}
             min="1"
             required
           />
