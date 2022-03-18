ffc76d0be4fc16590a66a452eb41e536738a2bef
Bradley Wong <blwong77@gmail.com>
2022-03-18 12:53:06 -0700

    added additional validation for checkDate

--- back-end/src/reservations/validation/checkDate.js
+++ back-end/src/reservations/validation/checkDate.js
@@ -1,10 +1,21 @@
 function checkDate(req, res, next) {
   const date = req.body.data.reservation_date;
 
-  if (date && String(new Date(date)) !== "Invalid Date") {
-    res.locals.reservationData.reservation_date = date;
-    return next();
+  if(date) {
+    const reservationDate = new Date(date);
+    const currentDate = new Date()
+    if(reservationDate.getUTCDay() === 2) {
+      return next({status:400, message: "The restaurant is closed on Tuesdays."})
+    }
+    if(reservationDate < currentDate) {
+      return next({status: 400, message: "Please make a reservation for today or a future date."})
+    }
+    if (String(new Date(date)) !== "Invalid Date") {
+      res.locals.reservationData.reservation_date = date;
+      return next();
+    }
   }
+
   next({ status: 400, message: "reservation_date is required" });
 }
 
