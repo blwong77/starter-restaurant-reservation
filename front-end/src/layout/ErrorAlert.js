c8854324465ed7d49393b7e5f5820a95f3bec60c
Bradley Wong <blwong77@gmail.com>
2022-03-18 12:28:00 -0700

    changed component to handle an array of errors

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -2,18 +2,17 @@ import React from "react";
 
 /**
  * Defines the alert message to render if the specified error is truthy.
- * @param error
+ * @param errors
  *  an instance of an object with `.message` property as a string, typically an Error instance.
  * @returns {JSX.Element}
  *  a bootstrap danger alert that contains the message string.
  */
 
-function ErrorAlert({ error }) {
-  return (
-    error && (
-      <div className="alert alert-danger m-2">Error: {error.message}</div>
-    )
-  );
+function ErrorAlert({ errors }) {
+  if (!errors) return null;
+  return errors.map((error) => {
+    return <div className="alert alert-danger m-2">Error: {error}</div>;
+  });
 }
 
 export default ErrorAlert;
