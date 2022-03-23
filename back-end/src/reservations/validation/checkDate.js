1f4e17a664039271f29df99125140f4da4d5848b
Bradley Wong <blwong77@gmail.com>
2022-03-23 12:36:53 -0700

    fixed date validation for same day reservations

--- back-end/src/reservations/validation/checkDate.js
+++ back-end/src/reservations/validation/checkDate.js
@@ -1,14 +1,25 @@
 function checkDate(req, res, next) {
   const date = req.body.data.reservation_date;
 
-  if(date) {
+  if (date) {
     const reservationDate = new Date(date);
-    const currentDate = new Date()
-    if(reservationDate.getUTCDay() === 2) {
-      return next({status:400, message: "The restaurant is closed on Tuesdays."})
+    const currentDate = new Date();
+    if (reservationDate.getUTCDay() === 2) {
+      return next({
+        status: 400,
+        message: "The restaurant is closed on Tuesdays.",
+      });
     }
-    if(reservationDate < currentDate) {
-      return next({status: 400, message: "Please make a reservation for today or a future date."})
+    if (
+      reservationDate.getUTCFullYear() < currentDate.getUTCFullYear &&
+      reservationDate.getUTCMonth() < currentDate.getUTCMonth() &&
+      reservationDate.getUTCDate() < currentDate.getUTCDate()
+    ) {
+      console.log(reservationDate, currentDate);
+      return next({
+        status: 400,
+        message: "Please make a reservation for today or a future date. Server",
+      });
     }
     if (String(new Date(date)) !== "Invalid Date") {
       res.locals.reservationData.reservation_date = date;
